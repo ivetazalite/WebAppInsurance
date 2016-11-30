fae1b3bfd7e38bb6ba0df526894474e68314fe4d
KRUROL <rolands.krumbergs@if.lv>
2016-11-30 15:04:01 +0200

    Add project files.

---
+++ Training.Programming.FinalTask/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Training.Programming.FinalTask.MvcApplication" Language="C#" %>
---
+++ Training.Programming.FinalTask/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Training.Programming.FinalTask.Startup))]
+namespace Training.Programming.FinalTask
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
